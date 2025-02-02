bfa0886aeb77cd96eb3d9718efd340393d551ebf
GAURJIsaheb <adityagaur086@gmail.com>
2025-02-02 14:01:27 +0530

    checking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import EventForm from './Pages/event-form'
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4">check check check
       {/* <h1 className="text-2xl font-bold mb-4">Shadcn UI Button Test</h1>
       <TestButton /> */}
       <EventForm />
